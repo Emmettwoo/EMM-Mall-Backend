bcbaa57a18fd6a6b5032388cf4f429654ebfe6ed
wuzhihong <wuzhihong@interlib.top>
2021-03-06 17:08:00 +0800

    minus

--- src/main/java/top/woohoo/mall/service/FileService.java
+++
@@ -1,9 +0,0 @@
-package top.woohoo.mall.service;
-
-import org.springframework.web.multipart.MultipartFile;
-import top.woohoo.mall.common.ServerResponse;
-
-public interface FileService {
-
-    ServerResponse<String> upload(MultipartFile file, String path);
-}
--- src/main/java/top/woohoo/mall/service/OrderItemService.java
+++
@@ -1,7 +0,0 @@
-package top.woohoo.mall.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import top.woohoo.mall.model.pojo.OrderItem;
-
-public interface OrderItemService extends IService<OrderItem> {
-}
